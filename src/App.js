e6397a3e1f5630f7ff1ba86f12fa621d855123a6
kraftkinder <innovation.haus1@gmail.com>
2024-07-08 19:24:51 +0200

    nur ein .env in gitignore app js mit $ im titel

--- src/App.js
+++ src/App.js
@@ -199,7 +199,7 @@ function App() {
     <ChakraProvider theme={theme}>
       <Box p={10} maxW="md" mx="auto" mt={8} color="brand.50">
         <Heading mb={4} as="h1" size="2xl" textAlign="left">
-          Brainy#
+          Brainy§
         </Heading>
         {syncMessage && <Text mt={2} color="gray.400" fontSize="sm">{syncMessage}</Text>}
         {!authenticated && (
